613fd5828c639cf50c9d0299d29bd59e092c81e1
Sparrow <2196442691@qq.com>
2023-10-20 12:28:59 +0800

    add DataDelete

---
+++ bitcask-go/data/data_file_test.go
@@ -0,0 +1,60 @@
+package data
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestOpenDataFile(t *testing.T) {
+	dataFile1, err := OpenDataFile(os.TempDir(), 0)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile1)
+
+	dataFile2, err := OpenDataFile(os.TempDir(), 111)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile2)
+
+	dataFile3, err := OpenDataFile(os.TempDir(), 0)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile3)
+}
+
+func TestDataFile_Write(t *testing.T) {
+	dataFile, err := OpenDataFile(os.TempDir(), 0)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa"))
+	assert.Nil(t, err)
+
+	err = dataFile.Write([]byte("bbb"))
+	assert.Nil(t, err)
+
+	err = dataFile.Write([]byte("ccc"))
+	assert.Nil(t, err)
+}
+
+func TestDataFile_Close(t *testing.T) {
+	dataFile, err := OpenDataFile(os.TempDir(), 0)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa"))
+	assert.Nil(t, err)
+
+	err = dataFile.Close()
+	assert.Nil(t, err)
+}
+
+func TestDataFile_Sync(t *testing.T) {
+	dataFile, err := OpenDataFile(os.TempDir(), 0)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa"))
+	assert.Nil(t, err)
+
+	err = dataFile.Sync()
+	assert.Nil(t, err)
+}
--- bitcask-go/db.go
+++ bitcask-go/db.go
@@ -86,6 +86,34 @@ func (db *DB) Put(key []byte, value []byte) error {
 	return nil
 }
 
+// Delete 根据 key 删除对应的数据
+func (db *DB) Delete(key []byte) error {
+	// 判断 key 的有效性
+	if len(key) == 0 {
+		return ErrKeyIsEmpty
+	}
+
+	// 检查 key 是否存在，如果不存在直接返回
+	if pos := db.index.Get(key); pos == nil {
+		return nil
+	}
+
+	// 构造 logRecord 信息，标识其是被删除的
+	logRecord := &data.LogRecord{Key: key, Type: data.LogRecordDeleted}
+	// 写入到数据文件中
+	_, err := db.appendLogRecord(logRecord)
+	if err != nil {
+		return nil
+	}
+
+	// 从内存索引中中删除对应的 key
+	ok := db.index.Delete(key)
+	if !ok {
+		return ErrIndexUpdateFailed
+	}
+	return nil
+}
+
 // Get 根据 key 读取数据
 func (db *DB) Get(key []byte) ([]byte, error) {
 	db.mu.Lock()
@@ -267,10 +295,15 @@ func (db *DB) loadIndexFromDataFile() error {
 
 			// 构造内存索引并保存
 			logRecordPos := &data.LogRecordPos{Fid: fileId, Offset: offset}
+			var ok bool
 			if lofRecord.Type == data.LogRecordDeleted {
-				db.index.Delete(lofRecord.Key)
+				ok = db.index.Delete(lofRecord.Key)
 			} else {
-				db.index.Put(lofRecord.Key, logRecordPos)
+				ok = db.index.Put(lofRecord.Key, logRecordPos)
+			}
+
+			if !ok {
+				return ErrIndexUpdateFailed
 			}
 
 			// 递增 offset，下一次从新的位置开始读
